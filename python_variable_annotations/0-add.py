8a8b4de44ff9726007e3229817299bbe630d01cd
Ghaida <alsabtighaida@gmail.com>
2026-08-28 19:35:23 +0300

    fix: pycodestyle 2 lines after module documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,5 +3,13 @@
 type-annotated function add that takes a
 float a and a float b as arguments and returns their sum as a float.
 """
+
+
 def add(a: float, b: float) -> float:
+    """
+    Args: a (float): first number
+          b (float): second number
+
+    Returns: float: sum of a and b
+    """
     return (a + b)
