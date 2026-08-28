5017a46edd92ac62a406b9e04127dec6e8aa4271
Ghaida <alsabtighaida@gmail.com>
2026-08-28 19:45:50 +0300

    add: task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+type-annotated function concat that takes a
+float number n as argument and returns the
+floor as int.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Args: n (float): float number
+
+    Returns: int: the floor of n
+    """
+    return int(n)
