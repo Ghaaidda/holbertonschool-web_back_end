2a89d6fcba5346ab3689af9e91fa8f7ba07519fd
Ghaida <alsabtighaida@gmail.com>
2026-08-28 19:39:08 +0300

    fix: trailing whitespace

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 type-annotated function concat that takes a
-string a and a string b as arguments and 
+string a and a string b as arguments and
 returns their concatenation as a string.
 """
 
