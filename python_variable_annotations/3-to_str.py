c98c98094a46c916bf29284ac51b29462d02d6a4
Ghaida <alsabtighaida@gmail.com>
2026-08-28 19:49:02 +0300

    add: task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str that takes a
+float number n as argument and returns the
+string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args: n (float): float number
+
+    Returns: str: the string representation of n
+    """
+    return str(n)
