6a8cbb2d852445dbb800a91da0523a0c9a8ed40a
Ghaida <alsabtighaida@gmail.com>
2026-08-29 17:18:51 +0300

    add: task 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+type-annotated async coroutine that takes an
+integer and waits for a random delay.
+"""
+
+
+import asyncio, random
+
+
+async def wait_random(max_delay: int = 10) -> int:
+    """
+    Args: max_delay (int): random number
+
+    Returns: int: the random number
+    """
+    await asyncio.sleep((float(max_delay)))
+    return max_delay
